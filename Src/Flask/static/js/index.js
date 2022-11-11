6b426007dad4990a7c5a845ce2ac3c07caef3a32
Wgkai <945044615@qq.com>
2022-11-11 20:22:34 +0800

    beautify homepage

--- Src/Flask/static/js/index.js
+++ Src/Flask/static/js/index.js
@@ -1,35 +1,31 @@
-gsap.registerPlugin(ScrollTrigger);
+// gsap.registerPlugin(ScrollTrigger);
 
-let panels = gsap.utils.toArray(".panel"),
-    scrollTween;
+// let panels = gsap.utils.toArray(".panel"),
+//     scrollTween;
 
-function goToSection(i) {
-  scrollTween = gsap.to(window, {
-    scrollTo: {y: i * innerHeight, autoKill: false},
-    duration: 1,
-    onComplete: () => scrollTween = null,
-    overwrite: true
-  });
-}
+// function goToSection(i) {
+//   scrollTween = gsap.to(window, {
+//     scrollTo: {y: i * innerHeight, autoKill: false},
+//     duration: 1,
+//     onComplete: () => scrollTween = null,
+//     overwrite: true
+//   });
+// }
 
-panels.forEach((panel, i) => {
-  ScrollTrigger.create({
-    trigger: panel,
-    start: "top bottom",
-    end: "+=200%",
-    onToggle: self => self.isActive && !scrollTween && goToSection(i)
-  });
-});
-
-// just in case the user forces the scroll to an inbetween spot (like a momentum scroll on a Mac that ends AFTER the scrollTo tween finishes):
-ScrollTrigger.create({
-  start: top, 
-  end: "max",
-  snap: 1 / (panels.length - 1)
-});
-
-
-// document.addEventListener('DOMContentLoaded', function () {
-//     window.setTimeout(document.querySelector('svg').classList.add('animated'),1000);
+// panels.forEach((panel, i) => {
+//   ScrollTrigger.create({
+//     trigger: panel,
+//     start: "top bottom",
+//     end: "+=200%",
+//     onToggle: self => self.isActive && !scrollTween && goToSection(i)
+//   });
 // });
+
+// // just in case the user forces the scroll to an inbetween spot (like a momentum scroll on a Mac that ends AFTER the scrollTo tween finishes):
+// ScrollTrigger.create({
+//   start: top, 
+//   end: "max",
+//   snap: 1 / (panels.length - 1)
+// });
+
 
