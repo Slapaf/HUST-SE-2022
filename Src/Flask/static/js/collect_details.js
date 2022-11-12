9ec63decede59973971733362bad10fc0400bc9d
Wgkai <945044615@qq.com>
2022-11-12 23:33:27 +0800

    daily debug

--- Src/Flask/static/js/collect_details.js
+++ Src/Flask/static/js/collect_details.js
@@ -207,3 +207,17 @@ function addmember(submitter_order_idx, submitter_name, submit_time, file_submit
         addfile(file_submitted_list);
     }
 }
+
+function getCollectionId() {
+    let url = document.location.toString();
+    console.log(url)
+    var arrUrl = url.split("//");
+    var start = arrUrl[1].lastIndexOf("/");
+    var relUrl = arrUrl[1].substring(start);//stop省略，截取从start开始到结尾的所有字符
+    if(relUrl.indexOf("?") != -1){
+        relUrl = relUrl.split("?")[0];
+    }
+    return relUrl;
+}
+
+console.log(getCollectionId());
